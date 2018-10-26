c88a7265332c9f7cad3c8820457337bad06ebe43
semifoliage <semifoliage@git.cloud.tencent.com>
2018-10-26 14:40:43 +0800

    comit 10.26_01
    commit scanreport

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,9 +1,25 @@
-const { uploader } = require('../qcloud')
+const { uploader } = require('../qcloud');
+var fs=require('fs');
+
+
 
 module.exports = async ctx => {
     // 获取上传之后的结果
     // 具体可以查看：
-    const data = await uploader(ctx.req)
+    //const data = await uploader(ctx.req);
 
-    ctx.state.data = data
+    var openId=ctx.request.query.openId;
+    var name=ctx.request.query.name;
+    var checkDate=ctx.request.query.date;
+    var timeStamp=new Date().getTime();
+    var path='img_tmp/'+openId+'_'+timeStamp+'_'+name+'_'+checkDate;
+    
+    var url=ctx.request.query.url;
+    var writerStream = fs.createWriteStream(path);
+
+    ctx.state.data = data;
+
+    ctx.response.type = 'html/text';
+  //responce body
+     ctx.body = ctx.req;
 }
